e9b9acf46607dc4db9f0341e2bc6c3b96b54ae0d
Stefanus Andre <stefanusandre07@gmail.com>
2026-06-12 13:34:48 +0700

    fix: add OPTIONS to cors AllowMethods

--- backend_absensi/main.go
+++ backend_absensi/main.go
@@ -50,7 +50,7 @@ func main() {
 	app.Use(logger.New())
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "*",
-		AllowMethods: "GET,POST,PUT,DELETE",
+		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 	}))
 
