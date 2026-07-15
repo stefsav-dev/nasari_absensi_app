55bf86df1072342376f81b029d3e655f56d23a40
stefsav-dev <stefsav2104@gmail.com>
2026-07-15 11:30:14 +0700

    Hapus backend_absensi dan frontend_absensi

--- frontend_absensi/app/admin/dashboard/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Dashboard Admin",
-};
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return children;
-}
--- frontend_absensi/app/admin/lokasi/tambah/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Tambah Lokasi",
-};
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return children;
-}
